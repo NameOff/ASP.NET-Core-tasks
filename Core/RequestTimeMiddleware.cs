1a9b3919f52629d167bb696d85666d63938c14cf
NameOff <alexeykotelnikov4@gmail.com>
2017-11-25 18:55:49 +0500

    middlewares task

---
+++ Core/RequestTimeMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Core
+{
+    public class RequestTimeMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public RequestTimeMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+            await next.Invoke(context);
+            await context.Response.WriteAsync($"Middleware class + function time: {timer.ElapsedMilliseconds / 1000.0}\n");
+        }
+    }
+}
---
+++ Core/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseMiddleware<RequestTimeMiddleware>();
+
+            app.Use(async (context, next) =>
+            {
+                var timer = new Stopwatch();
+                timer.Start();
+                timer.Stop();
+                await next.Invoke();
+                await context.Response.WriteAsync($"Middleware function time: {timer.ElapsedMilliseconds / 1000.0}\n");
+            });
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("done\n");
+            });
+        }
+    }
+}
