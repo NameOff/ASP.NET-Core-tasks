15e1c293ac2476058e59fd67a502ae1c64a5c6c7
NameOff <alexeykotelnikov4@gmail.com>
2017-11-30 01:34:56 +0500

    added config file

---
+++ Core/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Students()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
--- Core/Startup.cs
+++ Core/Startup.cs
@@ -1,22 +1,33 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading;
+using Core.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Core
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<StudentsContext>(options =>
+                options.UseSqlServer(connection));
+            services.AddMvc();
+        }
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
